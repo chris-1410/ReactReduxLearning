c03ff069a105a5e68cc2f98cbb04bf36b913f5f2
chris-1410 <christopherraj@jmangroup.com>
2023-01-20 19:00:58 +0530

    Redux Learning

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./Components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./Components/ProductListing";
+import ProductDetail from "./Components/ProductDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route> 404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
