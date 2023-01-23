0192416535949246a7b2beeb1194454184f9cad3
chris-1410 <christopherraj@jmangroup.com>
2023-01-23 18:11:53 +0530

    Experimented with Basic Redux operations

--- src/Redux/actions/productActions.js
+++ src/Redux/actions/productActions.js
@@ -8,9 +8,14 @@ export const setProducts = (products) => {
 };
 
 export const selectedProduct = (product) => {
-    return {
-      type: ActionTypes.SELECTED_PRODUCT,
-      payload: product,
-    };
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product,
   };
-  
+};
+
+export const removeSelectedProduct = () => {
+  return {
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+  };
+};
